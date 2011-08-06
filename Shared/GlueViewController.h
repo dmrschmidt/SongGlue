6579e9a9cc6df14b5389cdfd24fc3e5e351fc539
Dennis Schmidt <dmrschmidt@gmail.com>
2011-08-06 14:04:28 +0200

    refactorings

--- Shared/GlueViewController.h
+++ Shared/GlueViewController.h
@@ -7,7 +7,74 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
+#import <AVFoundation/AVFoundation.h>
+#import "GlueView.h"
+#import "InfiniteScrollView.h"
 
-@interface GlueViewController : UIViewController
+@interface GlueViewController : UIViewController <UIAccelerometerDelegate, UIScrollViewDelegate> {
+    // common stuff
+    NSArray *_itemsFromGenericQuery;
+    NSArray *_audioSamples;
+    
+    // scrolling stuff
+    UIScrollView *_scrollView;
+    NSMutableSet *_visiblePages;
+    NSMutableSet *_recycledPages;
+    
+    // accelerometer stuff
+    UIAccelerometer *_accelerometer;
+    NSMutableArray *_history_x;
+    NSMutableArray *_history_y;
+    NSMutableArray *_history_z;
+}
+
+/*
+ * Returns a random title from the iPod.
+ */
+- (NSString *)randomTitle;
+
+/*
+ * deprecated
+ */
+- (NSUInteger)glueCount;
+
+/*
+ *
+ */
+- (GlueView *)dequeRecycledPage;
+
+/*
+ *
+ */
+- (BOOL)isDisplayingPageForIndex:(NSUInteger)index;
+
+/*
+ *
+ */
+- (void)tilePages;
+
+/*
+ *
+ */
+- (IBAction)updateTitle:(id)sender;
+
+/*
+ * Plays a random audio sample (Bobby's hit sound).
+ */
+- (void)playAudio;
+
+@property(nonatomic, retain) NSArray *itemsFromGenericQuery;
+@property(nonatomic, retain) NSArray *audioSamples;
+
+@property(nonatomic, retain) UIScrollView *scrollView;
+@property(nonatomic, retain) NSMutableSet *visiblePages;
+@property(nonatomic, retain) NSMutableSet *recycledPages;
+
+@property(nonatomic, retain) UIAccelerometer *accelerometer;
+@property(nonatomic, retain) NSMutableArray *history_x;
+@property(nonatomic, retain) NSMutableArray *history_y;
+@property(nonatomic, retain) NSMutableArray *history_z;
+
 
 @end
--- Shared/LocalGlueViewController.h
+++ Shared/LocalGlueViewController.h
@@ -7,52 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <MediaPlayer/MediaPlayer.h>
-#import <AVFoundation/AVFoundation.h>
-#import "GlueView.h"
-#import "InfiniteScrollView.h"
+#import "GlueViewController.h"
 
-@interface LocalGlueViewController : UIViewController <UIAccelerometerDelegate, UIScrollViewDelegate> {
-    UILabel *_labelView;
-    UIButton *_buttonView;
-    NSArray *_itemsFromGenericQuery;
-    NSArray *_audioSamples;
+@interface LocalGlueViewController : GlueViewController {
     
-    // scrolling stuff
-    UIScrollView *_scrollView;
-    NSMutableSet *_visiblePages;
-    NSMutableSet *_recycledPages;
-    
-    // accelerometer stuff
-    UIAccelerometer *_accelerometer;
-    NSMutableArray *_history_x;
-    NSMutableArray *_history_y;
-    NSMutableArray *_history_z;
 }
-
-- (NSString *)randomTitle;
-- (NSUInteger)glueCount;
-- (GlueView *)dequeRecycledPage;
-- (BOOL)isDisplayingPageForIndex:(NSUInteger)index;
-- (void)initScrollView;
-- (void)tilePages;
-- (void)initAccelerometer;
-- (IBAction)updateTitle:(id)sender;
-- (void)playAudio;
-
-@property(nonatomic, retain) IBOutlet UILabel *labelView;
-@property(nonatomic, retain) IBOutlet UIButton *buttonView;
-@property(nonatomic, retain) NSArray *itemsFromGenericQuery;
-
-@property(nonatomic, retain) UIScrollView *scrollView;
-@property(nonatomic, retain) NSMutableSet *visiblePages;
-@property(nonatomic, retain) NSMutableSet *recycledPages;
-
-@property(nonatomic, retain) NSArray *audioSamples;
-
-@property(nonatomic, retain) UIAccelerometer *accelerometer;
-@property(nonatomic, retain) NSMutableArray *history_x;
-@property(nonatomic, retain) NSMutableArray *history_y;
-@property(nonatomic, retain) NSMutableArray *history_z;
 
 @end
